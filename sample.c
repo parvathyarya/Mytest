c5b3d16392844dd04e19e4c77a4ec27a7ca21c8c
parvathyarya <37532869+parvathyarya@users.noreply.github.com>
2018-03-26 14:15:46 +0530

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,2 +1,3 @@
 You can choose to make the repository either public or private. Public repositories are visible to the public, while private repositories are only accessible to you, and people you share them with. 
 Getting a Git Repository. You can get a Git project using two main approaches.
+The first takes an existing project or directory and imports it into Git.
